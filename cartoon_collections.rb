6783dce37d6f789c4642b97075e3092e81db4414
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-11 13:10:13 -0600

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  i = 1
+  array.each do |name|
+    puts "#{i}. #{name}"
+    i += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map do |call|
+    "#{call.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(short_words)# code an argument here
   # Your code here
+  # short_words.any? do |word|
+  #   word.length > 4
+  # end
+  short_words.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  #find will return the first element that eval condition to true
+  snacks.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
